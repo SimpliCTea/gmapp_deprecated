0472de18a84a8782859d45645b0e2a520a299030
Marc-Leon Römer <marcleonroemer@gmx.net>
2019-08-11 02:19:37 +0200

    feat: Create RollTable Component

---
+++ src/rollTables/RollTable.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class RollTable extends Component {
+    render() {
+        return (
+            <div>
+                Here comes a RollTable!
+            </div>
+        )
+    }
+}
